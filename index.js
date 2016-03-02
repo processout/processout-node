423747dd463b0ae3129572685a6bee5d36e01d39
Manuel HUEZ <manuel.huez@gmail.com>
2016-03-02 11:19:05 +0100

    Update library to version 3.0.0-1

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    ProcessOut: require('./lib/processout'),
+    Customer: require('./lib/customer'),
+    CustomerAction: require('./lib/customeraction'),
+    CustomerToken: require('./lib/customertoken'),
+    Event: require('./lib/event'),
+    Invoice: require('./lib/invoice'),
+    PaymentGateway: require('./lib/paymentgateway'),
+    PaymentGatewayPublicKey: require('./lib/paymentgatewaypublickey'),
+    Project: require('./lib/project'),
+    RecurringInvoice: require('./lib/recurringinvoice'),
+    TailoredInvoice: require('./lib/tailoredinvoice'),
+    
+};
