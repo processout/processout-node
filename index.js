131f4a4031132796937c5e1286c45cc5bf043113
Manuel HUEZ <Manu@MacBook-Pro-6.local>
2016-05-23 11:28:14 +0200

    Add missing fields to recurring invoice

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 module.exports = {
     ProcessOut: require('./lib/processout'),
-    Customer: require('./lib/customer'),
-    CustomerAction: require('./lib/customeraction'),
-    CustomerToken: require('./lib/customertoken'),
-    Event: require('./lib/event'),
     Invoice: require('./lib/invoice'),
-    PaymentGateway: require('./lib/paymentgateway'),
-    PaymentGatewayPublicKey: require('./lib/paymentgatewaypublickey'),
-    Project: require('./lib/project'),
     RecurringInvoice: require('./lib/recurringinvoice'),
     TailoredInvoice: require('./lib/tailoredinvoice'),
+    Customer: require('./lib/customer'),
+    CustomerToken: require('./lib/customertoken'),
+    CustomerAction: require('./lib/customeraction'),
+    Event: require('./lib/event'),
+    Project: require('./lib/project'),
+    PaymentGateway: require('./lib/paymentgateway'),
+    PaymentGatewayPublicKey: require('./lib/paymentgatewaypublickey'),
     
     Webhook: require('./lib/webhook'),
 };
