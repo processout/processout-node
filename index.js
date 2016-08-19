7da7b1040a2caaf3c1452ef72df660749e77900f
Manuel HUEZ <Manu@MacBook-Pro-6.local>
2016-08-19 16:49:27 -0400

    Update customer attributes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 module.exports = {
     ProcessOut: require('./lib/processout'),
+    Activity: require('./lib/activity'),
     AuthorizationRequest: require('./lib/authorizationrequest'),
     Customer: require('./lib/customer'),
     Token: require('./lib/token'),
     Event: require('./lib/event'),
     Invoice: require('./lib/invoice'),
-    Activity: require('./lib/activity'),
     RecurringInvoice: require('./lib/recurringinvoice'),
     TailoredInvoice: require('./lib/tailoredinvoice'),
     Transaction: require('./lib/transaction'),
