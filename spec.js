862610dd3292e427adc91bbc597c4801cd7aa5e4
Manuel Huez <manuel@Manuels-MacBook-Pro.local>
2016-12-12 21:12:11 -0500

    Improve error code support

--- spec.js
+++ spec.js
@@ -6,22 +6,29 @@ var processout = require('.');
 var client = new processout.ProcessOut('proj_gAO1Uu0ysZJvDuUpOGPkUBeE3pGalk3x', 
     'key_fBjPvkgT8gyKc1SUpy0PfjL7UgsRmUug');
 
+function expect(val, message) {
+    if (!val) {
+        console.log('FAIL: '+message);
+        process.exit(1);
+    }
+}
+
 // Create and fetch a new invoice
 client.newInvoice({
     'name': 'Test invoice',
     'amount': '9.99',
     'currency': 'USD'
 }).create().then(function(invoice) {
-    assert(invoice.getId() != '', 'The invoice ID should not be empty');
+    expect(invoice.getId() != '', 'The invoice ID should not be empty');
 
     client.newInvoice().find(invoice.getId()).then(function(fetched) {
-        assert(fetched.getId() != '', 'The fetched invoice ID should not be empty');
-        assert(invoice.getId() == fetched.getId(), 'The invoices ID should be equal');
+        expect(fetched.getId() != '', 'The fetched invoice ID should not be empty');
+        expect(invoice.getId() == fetched.getId(), 'The invoices ID should be equal');
     }, function(err) {
-        assert(false, 'There should not be any error');
+        expect(false, 'There should not be any error');
     });
 }, function(err) {
-    assert(false, 'There should not be any error');
+    expect(false, 'There should not be any error');
 });
 
 
@@ -29,12 +36,12 @@ client.newInvoice({
 client.newCustomer().all().then(function(customers) {
     //
 }, function(err) {
-    assert(false, 'There should not be any error');
+    expect(false, 'There should not be any error');
 });
 
 // Create a subscription for a customer
 client.newCustomer().create(function(customer) {
-    assert(customer.getId() != '', 'The created customer ID should not be empty');
+    expect(customer.getId() != '', 'The created customer ID should not be empty');
 
     client.newSubscription({
         'amount': '9.99',
@@ -42,24 +49,31 @@ client.newCustomer().create(function(customer) {
         'interval': '1d',
         'name': 'great subscription'
     }).create(customer.getId()).then(function(subscription) {
-        assert(subscription.getId() != '', 'The created subscription ID should not be empty');
+        expect(subscription.getId() != '', 'The created subscription ID should not be empty');
     }, function(err) {
-        assert(false ,'There should not be any error');
+        expect(false, 'There should not be any error');
     });
 }, function(err) {
-    assert(false ,'There should not be any error');
+    expect(false, 'There should not be any error');
 });
 
 // Expand a customers' project and fetch gateways
 var options = {expand: ["project"]};
-client.newCustomer().create(function(customer, options) {
-    assert(customer.getProject(), 'The invoice project could not be expanded');
+client.newCustomer().create(options).then(function(customer) {
+    expect(customer.getProject(), 'The invoice project could not be expanded');
 
     customer.getProject().fetchGatewayConfigurations().then(function(conf) {
         //
     }, function(err) {
-        assert(false ,'There should not be any error');
+        expect(false, 'There should not be any error');
     });
 }, function(err) {
-    assert(false ,'There should not be any error');
+    expect(false, 'There should not be any error');
+});
+
+// Make sure the error code is correctly set
+client.newCustomer().find("bad").then(function(customer) {
+    expect(false, 'There should have been an error');
+}, function(err) {
+    expect(err.code == 'resource.customer.not-found', 'The error code was incorrect: '+err.code);
 });
