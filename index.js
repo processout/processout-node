fc1fcc54931012e1ee56f3e55820386c1399fa08
Manuel Huez <manuel@Manuels-MacBook-Pro.local>
2017-03-17 17:43:48 +0100

    Add support for addons. Improve pagination. Simplify function signatures. Bump to API version 1.4.0.0

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 module.exports = {
     ProcessOut: require('./lib/processout'),
     Activity: require('./lib/activity'),
+    Addon: require('./lib/addon'),
+    APIRequest: require('./lib/apirequest'),
+    APIVersion: require('./lib/apiversion'),
     AuthorizationRequest: require('./lib/authorizationrequest'),
     Card: require('./lib/card'),
     CardInformation: require('./lib/cardinformation'),
@@ -16,13 +19,16 @@ module.exports = {
     Invoice: require('./lib/invoice'),
     InvoiceDetail: require('./lib/invoicedetail'),
     CustomerAction: require('./lib/customeraction'),
+    DunningAction: require('./lib/dunningaction'),
     Plan: require('./lib/plan'),
     Product: require('./lib/product'),
     Project: require('./lib/project'),
     Refund: require('./lib/refund'),
     Subscription: require('./lib/subscription'),
     Transaction: require('./lib/transaction'),
+    TransactionOperation: require('./lib/transactionoperation'),
     Webhook: require('./lib/webhook'),
+    WebhookEndpoint: require('./lib/webhookendpoint'),
     
     GatewayRequest: require('./lib/gatewayrequest')
 };
