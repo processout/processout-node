eb019fe5544eee1b8ef1492b5811eb25ea0a1c12
ProcessOut Fountain <internal@processout.com>
2020-01-09 18:21:57 +0100

    Bump version to 7.3.5

--- src/networking/request.ts
+++ src/networking/request.ts
@@ -36,7 +36,7 @@ class Request {
             req.headers = {};
 
         req.headers['API-Version'] = '1.4.0.0';
-        req.headers['User-Agent'] = 'ProcessOut NodeJS-Bindings/7.3.4';
+        req.headers['User-Agent'] = 'ProcessOut NodeJS-Bindings/7.3.5';
 
         if (typeof options === 'undefined')
             return req;
